4d98cd0699e82b5c2187c64de6770372df029366
hectorkutleng <hectorh@kutleng.co.za>
2018-06-02 16:44:30 +0200

    Cosmetic changes

--- casperbsp/sources/vhdl/rtl/ringbuffer/packetramsp.vhd
+++ casperbsp/sources/vhdl/rtl/ringbuffer/packetramsp.vhd
@@ -60,43 +60,43 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity packetramsp is
-	generic(
-		G_ADDR_WIDTH : natural := 8 + 2;
-		G_DATA_WIDTH : natural := 64
-	);
-	port(
-		Clk          : in  STD_LOGIC;
-		WriteAddress : in  STD_LOGIC_VECTOR(G_ADDR_WIDTH - 1 downto 0);
-		ReadAddress  : in  STD_LOGIC_VECTOR(G_ADDR_WIDTH - 1 downto 0);
-		WriteEnable  : in  STD_LOGIC;
-		ReadEnable   : in  STD_LOGIC;
-		WriteData    : in  STD_LOGIC_VECTOR(G_DATA_WIDTH - 1 downto 0);
-		ReadData     : out STD_LOGIC_VECTOR(G_DATA_WIDTH - 1 downto 0)
-	);
+    generic(
+        G_ADDR_WIDTH : natural := 8 + 2;
+        G_DATA_WIDTH : natural := 64
+    ); 
+    port(
+        Clk          : in  STD_LOGIC;
+        WriteAddress : in  STD_LOGIC_VECTOR(G_ADDR_WIDTH - 1 downto 0);
+        ReadAddress  : in  STD_LOGIC_VECTOR(G_ADDR_WIDTH - 1 downto 0);
+        WriteEnable  : in  STD_LOGIC;
+        ReadEnable   : in  STD_LOGIC;
+        WriteData    : in  STD_LOGIC_VECTOR(G_DATA_WIDTH - 1 downto 0);
+        ReadData     : out STD_LOGIC_VECTOR(G_DATA_WIDTH - 1 downto 0)
+    );
 end entity packetramsp;
 
 architecture rtl of packetramsp is
-	-- Declaration of ram signals
-	type PacketRAM_t is array ((2**G_ADDR_WIDTH) - 1 downto 0) of std_logic_vector(G_DATA_WIDTH - 1 downto 0);
-	signal RAMData : PacketRAM_t;
+    -- Declaration of ram signals
+    type PacketRAM_t is array ((2**G_ADDR_WIDTH) - 1 downto 0) of std_logic_vector(G_DATA_WIDTH - 1 downto 0);
+    signal RAMData : PacketRAM_t;
 begin
 
-	RAMWrite : process(Clk)
-	begin
-		if rising_edge(Clk) then
-			if (WriteEnable = '1') then
-				RAMData(to_integer(unsigned(WriteAddress))) <= WriteData;
-			end if;
-		end if;
-	end process RAMWrite;
+    RAMWrite : process(Clk)
+    begin
+        if rising_edge(Clk) then
+            if (WriteEnable = '1') then
+                RAMData(to_integer(unsigned(WriteAddress))) <= WriteData;
+            end if;
+        end if;
+    end process RAMWrite;
 
-	RAMRead : process(Clk)
-	begin
-		if rising_edge(Clk) then
-			if (ReadEnable = '1') then
-				ReadData <= RAMData(to_integer(unsigned(ReadAddress)));
-			end if;
-		end if;
-	end process RAMRead;
+    RAMRead : process(Clk)
+    begin
+        if rising_edge(Clk) then
+            if (ReadEnable = '1') then
+                ReadData <= RAMData(to_integer(unsigned(ReadAddress)));
+            end if;
+        end if;
+    end process RAMRead;
 
 end architecture rtl;
