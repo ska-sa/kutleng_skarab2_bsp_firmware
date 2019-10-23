7dd067b8a3a0e266216e814753dfe2ba41a2ae51
Benjamin Hector Hlophe <hectorh@kutleng.co.za>
2019-10-23 08:10:57 +0200

    Added functionality for UDP data stripper to receive data and stream it over AXI-S bus to Yelloblock Interface

--- casperbsp/sources/vhdl/rtl/udp/udpdatastripper.vhd
+++ casperbsp/sources/vhdl/rtl/udp/udpdatastripper.vhd
@@ -52,7 +52,7 @@
 -- Tool Versions    : N/A                                                      -
 -- Description      : This module performs data streaming over UDP             -
 --                                                                             -
--- Dependencies     : macifudpserver,udpdatastripper,udpdatapacker             -
+-- Dependencies     : N/A                                                      -
 -- Revision History : V1.0 - Initial design                                    -
 --------------------------------------------------------------------------------
 library ieee;
@@ -60,42 +60,125 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity udpdatastripper is
-    generic(
-        G_SLOT_WIDTH : natural := 4;
-        G_ADDR_WIDTH : natural := 5
-    );
-    port(
-        axis_clk                     : in  STD_LOGIC;
-        axis_app_clk                 : in  STD_LOGIC;
-        axis_reset                   : in  STD_LOGIC;
-        EthernetMACEnable            : in  STD_LOGIC;
-        SetMACPromiscousMode         : in  STD_LOGIC;
-        RXOverFlowCount              : out STD_LOGIC_VECTOR(31 downto 0);
-        RXAlmostFullCount            : out STD_LOGIC_VECTOR(31 downto 0);
-        -- Packet Readout in addressed bus format
-        RecvRingBufferSlotID         : out STD_LOGIC_VECTOR(G_SLOT_WIDTH - 1 downto 0);
-        RecvRingBufferSlotClear      : out STD_LOGIC;
-        RecvRingBufferSlotStatus     : in  STD_LOGIC;
-        RecvRingBufferSlotTypeStatus : in  STD_LOGIC;
-        RecvRingBufferSlotsFilled    : in  STD_LOGIC_VECTOR(G_SLOT_WIDTH - 1 downto 0);
-        RecvRingBufferDataRead       : out STD_LOGIC;
-        -- Enable[0] is a special bit (we assume always 1 when packet is valid)
-        -- we use it to save TLAST
-        RecvRingBufferDataEnable     : in  STD_LOGIC_VECTOR(63 downto 0);
-        RecvRingBufferDataOut        : in  STD_LOGIC_VECTOR(511 downto 0);
-        RecvRingBufferAddress        : out STD_LOGIC_VECTOR(G_ADDR_WIDTH - 1 downto 0);
-        --
-        axis_tuser                   : out STD_LOGIC;
-        axis_tdata                   : out STD_LOGIC_VECTOR(511 downto 0);
-        axis_tvalid                  : out STD_LOGIC;
-        axis_tready                  : in  STD_LOGIC;
-        axis_tkeep                   : out STD_LOGIC_VECTOR(63 downto 0);
-        axis_tlast                   : out STD_LOGIC
-    );
-
+	generic(
+		G_SLOT_WIDTH : natural := 4;
+		G_ADDR_WIDTH : natural := 5
+	);
+	port(
+		axis_clk                     : in  STD_LOGIC;
+		axis_reset                   : in  STD_LOGIC;
+		EthernetMACEnable            : in  STD_LOGIC;
+		-- Packet Readout in addressed bus format
+		RecvRingBufferSlotID         : out STD_LOGIC_VECTOR(G_SLOT_WIDTH - 1 downto 0);
+		RecvRingBufferSlotClear      : out STD_LOGIC;
+		RecvRingBufferSlotStatus     : in  STD_LOGIC;
+		RecvRingBufferDataRead       : out STD_LOGIC;
+		-- Enable[0] is a special bit (we assume always 1 when packet is valid)
+		-- we use it to save TLAST
+		RecvRingBufferDataEnable     : in  STD_LOGIC_VECTOR(63 downto 0);
+		RecvRingBufferDataOut        : in  STD_LOGIC_VECTOR(511 downto 0);
+		RecvRingBufferAddress        : out STD_LOGIC_VECTOR(G_ADDR_WIDTH - 1 downto 0);
+		--
+		axis_tuser                   : out STD_LOGIC;
+		axis_tdata                   : out STD_LOGIC_VECTOR(511 downto 0);
+		axis_tvalid                  : out STD_LOGIC;
+		axis_tready                  : in  STD_LOGIC;
+		axis_tkeep                   : out STD_LOGIC_VECTOR(63 downto 0);
+		axis_tlast                   : out STD_LOGIC
+	);
 end entity udpdatastripper;
 
 architecture rtl of udpdatastripper is
+	type UDPDataStripperSM_t is (
+		InitialiseSt,                   -- On the reset state
+		CheckEmptySlotSt,
+		ExtractPacketDataSt,
+		NextSlotSt
+	);
+	constant C_ADDRESS_MAX        : natural             := (2**G_ADDR_WIDTH) - 1;
+	signal StateVariable          : UDPDataStripperSM_t := InitialiseSt;
+	signal lRecvRingBufferAddress : unsigned(G_ADDR_WIDTH - 1 downto 0);
+	signal lRecvRingBufferSlotID  : unsigned(G_SLOT_WIDTH - 1 downto 0);
 begin
+	RecvRingBufferAddress <= std_logic_vector(lRecvRingBufferAddress);
+	RecvRingBufferSlotID  <= std_logic_vector(lRecvRingBufferSlotID);
+
+	SynchStateProc : process(axis_clk)
+	begin
+		if rising_edge(axis_clk) then
+			if (axis_reset = '1') then
+
+				StateVariable <= InitialiseSt;
+			else
+				case (StateVariable) is
+
+					when InitialiseSt =>
+
+						-- Wait for packet after initialization
+						StateVariable          <= CheckEmptySlotSt;
+						lRecvRingBufferSlotID  <= (others => '0');
+						lRecvRingBufferAddress <= (others => '0');
+
+					when CheckEmptySlotSt =>
+						if ((RecvRingBufferSlotStatus = '1') and (EthernetMACEnable ='1')) then
+							-- There is a packet waiting on the ring buffer
+							-- Start from the base address to extract the packet
+							lRecvRingBufferAddress <= (others => '0');
+							RecvRingBufferDataRead <= '1';
+							StateVariable          <= ExtractPacketDataSt;
+						else
+
+							RecvRingBufferDataRead <= '0';
+							-- Keep searching for a packet
+							StateVariable          <= CheckEmptySlotSt;
+						end if;
+
+					when ExtractPacketDataSt =>
+
+						RecvRingBufferDataRead <= '1';
+						if (axis_tready = '1') then
+							-- Read and point to next address
+							lRecvRingBufferAddress  <= lRecvRingBufferAddress + 1;
+							axis_tvalid             <= '1';
+							axis_tdata              <= RecvRingBufferDataOut;
+							axis_tkeep(63 downto 1) <= RecvRingBufferDataEnable(63 downto 1);
+							axis_tkeep(0)           <= '1';
+
+							if ((RecvRingBufferDataEnable(0) = '1') or (lRecvRingBufferAddress = C_ADDRESS_MAX)) then
+								-- This is the last byte to terminate the transaction
+								-- This is a wrap around condition just terminate the transaction
+								RecvRingBufferSlotClear <= '1';
+								axis_tlast              <= '1';
+								if (lRecvRingBufferAddress = C_ADDRESS_MAX) then
+									axis_tuser <= '1';
+								else
+									axis_tuser <= '0';
+								end if;
+								StateVariable           <= NextSlotSt;
+							else
+								axis_tlast    <= '0';
+								StateVariable <= ExtractPacketDataSt;
+							end if;
+						else
+							axis_tvalid   <= '0';
+							StateVariable <= ExtractPacketDataSt;
+						end if;
+
+					when NextSlotSt =>
+						axis_tlast              <= '0';
+						axis_tuser              <= '0';
+						axis_tvalid             <= '0';
+						RecvRingBufferDataRead  <= '0';
+						RecvRingBufferSlotClear <= '0';
+						-- Search next slots  
+						lRecvRingBufferSlotID   <= lRecvRingBufferSlotID + 1;
+						StateVariable           <= CheckEmptySlotSt;
+						
+					when others =>
+						StateVariable <= InitialiseSt;
+				end case;
+			end if;
+		end if;
+	end process SynchStateProc;
 
 end architecture rtl;
